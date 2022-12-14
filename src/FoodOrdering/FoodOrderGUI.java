2e250f8a8296b9cf57af2150a2372c602276a025
Rhoy Alvin Quilapio <dontmisstheblock@gmail.com>
2022-12-14 22:16:59 +0800

    Added Food Ordering system main function

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,8 +1,10 @@
 package FoodOrdering;
 
+import LeapYear.LeapYearGUI;
+
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame{
     private JPanel pnlMain;
     private JButton btnOrder;
     private JCheckBox cPizza;
@@ -15,4 +17,21 @@ public class FoodOrderGUI {
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+
+    public static void main (String[] args){
+        FoodOrderGUI foodOrderGUI = new FoodOrderGUI();
+        foodOrderGUI.setContentPane(foodOrderGUI.pnlMain);
+        foodOrderGUI.setSize(700, 700);
+        foodOrderGUI.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        foodOrderGUI.setVisible(true);
+        foodOrderGUI.setTitle("Food Ordering System");
+    }
+
+
+
+
+
+
+
+
 }
