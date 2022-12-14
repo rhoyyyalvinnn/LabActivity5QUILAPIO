a04899a864fca262bbaafe54186db00d95f03911
Rhoy Alvin Quilapio <dontmisstheblock@gmail.com>
2022-12-14 16:19:34 +0800

    Complete Leap Year

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,6 +1,7 @@
 package LeapYear;
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI extends JFrame{
     private JPanel pnlMain;
@@ -8,17 +9,46 @@ public class LeapYearGUI extends JFrame{
     private JButton btnCheckYear;
 
 
-    public static void main(String[] args) {
-        LeapYearGUI app = new LeapYearGUI();
-        app.setContentPane(app.pnlMain);
-        app.setSize(700, 700);
-        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        app.setVisible(true);
+        public static void main (String[] args){
+        LeapYearGUI leapyearGUI = new LeapYearGUI();
+        leapyearGUI.setContentPane(leapyearGUI.pnlMain);
+        leapyearGUI.setSize(700, 700);
+        leapyearGUI.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        leapyearGUI.setVisible(true);
+        leapyearGUI.setTitle("Leap Year Checker");
     }
 
 
+        public int LeapYearChecker () {
 
+                int year = Integer.parseInt(tfYear.getText());
 
-}
+                if (year % 4 == 0 && year % 100 != 0) {
+                    return 1;
+                } else if (year % 4 == 0 && year % 100 == 0) {
+                    return 0;
+                } else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+                    return 1;
+                }
+                return 0;
+    }
+
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (LeapYearChecker() == 1) {
+                    JOptionPane.showMessageDialog(pnlMain, "Leap Year");
+                } else if(LeapYearChecker() == 0){
+                    JOptionPane.showMessageDialog(pnlMain, "Not a Leap Year");
+                } else if(LeapYearChecker() == 3){
+                    JOptionPane.showMessageDialog(pnlMain, "Invalid Input");
+                }
+            }
+        });
+    }
+
+    }
+
 
 
