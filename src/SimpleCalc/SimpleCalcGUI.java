410e80e2c1fe1a48703c4818d7c77ae24c3b063c
Rhoy Alvin Quilapio <dontmisstheblock@gmail.com>
2022-12-14 17:10:38 +0800

    Simple Calulator Complete

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,11 +3,13 @@ package SimpleCalc;
 import LeapYear.LeapYearGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel pnlMain;
     private JTextField tfNumber1;
-    private JComboBox comboBox1;
+    private JComboBox cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
@@ -22,9 +24,43 @@ public class SimpleCalcGUI extends JFrame{
     }
 
 
+    public SimpleCalcGUI(){
 
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(cbOperations.getSelectedItem() == "+")
+                {
+                    int num1 = Integer.parseInt(tfNumber1.getText());
+                    int num2 = Integer.parseInt(tfNumber2.getText());
+                    int sum = num1+num2;
+                    lblResult.setText(String.valueOf(sum));
+                } else if(cbOperations.getSelectedItem() == "-")
+                {
+                    int num1 = Integer.parseInt(tfNumber1.getText());
+                    int num2 = Integer.parseInt(tfNumber2.getText());
+                    int diff = num1-num2;
+                    lblResult.setText(String.valueOf(diff));
+                } else if(cbOperations.getSelectedItem() == "*")
+                {
+                    int num1 = Integer.parseInt(tfNumber1.getText());
+                    int num2 = Integer.parseInt(tfNumber2.getText());
+                    int prod = num1*num2;
+                    lblResult.setText(String.valueOf(prod));
+                } else if(cbOperations.getSelectedItem() == "/")
+                {
 
+                    double num1 = Double.parseDouble(tfNumber1.getText());
+                    double num2 = Double.parseDouble(tfNumber2.getText());
+                    if(num2 == 0){
+                        JOptionPane.showMessageDialog(pnlMain, "Cannot be divided by Zero");
+                    }else {
+                        double quotient = num1/num2;
+                        lblResult.setText(String.valueOf(quotient));
+                    }
 
-
-
+                }
+            }
+        });
+    }
 }
