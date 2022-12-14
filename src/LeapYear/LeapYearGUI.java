1e391f324f6a46aecf1e19cb7eb0e22015bd4c05
Rhoy Alvin Quilapio <dontmisstheblock@gmail.com>
2022-12-14 15:22:32 +0800

    Added Main function

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -8,6 +8,13 @@ public class LeapYearGUI extends JFrame{
     private JButton btnCheckYear;
 
 
+    public static void main(String[] args) {
+        LeapYearGUI app = new LeapYearGUI();
+        app.setContentPane(app.pnlMain);
+        app.setSize(700, 700);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 
 
 
